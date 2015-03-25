1bcfe41c78b49dbc75d4fa64f56b228806819d67
jnyryan <jnyryan@gmail.com>
2015-03-25 11:43:39 +0000

    added demo project

---
+++ demo/demo.js
@@ -0,0 +1,25 @@
+(function() {
+  var logSuppress = require('../lib/log-suppress.js');
+	'use strict';
+
+  console.log("This demo will print 100 messages to the console");
+  console.log("Log-Suppress will be enabled after 5 messages");
+  console.log("All further output will be suppressed");
+
+  for(i=0;i<100;i++){
+    console.log("I am console output");
+    if(i==5)
+    {
+      console.log("***Turning Log-Suppress ON");
+      console.log("***All further output will be suppressed");
+      logSuppress.init(console);
+    }
+  }
+
+  console.log("You should not see me");
+  console.log("You should not see me");
+  console.log("You should not see me");
+  console.log("You should not see me");
+  console.log("You should not see me");
+
+})();
